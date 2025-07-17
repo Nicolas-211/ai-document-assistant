98231dd13cca0f3a5db577cf9da77b2310d9e35b
nicolas nanjari <nicolascanales216@gmail.com>
2025-07-17 16:59:17 -0400

    src document

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { DocumentModule } from './document/document.module';
 @Module({
-  imports: [AuthModule, PrismaModule,ConfigModule.forRoot({ isGlobal: true })],
+  imports: [AuthModule, PrismaModule,ConfigModule.forRoot({ isGlobal: true }), DocumentModule],
   controllers: [AppController],
   providers: [AppService],
 })
